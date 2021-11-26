e7c877b947847cb56472b5b02c7af42226404ff2
Lech Gudalewicz <lechgu@gmail.com>
2021-11-26 04:12:24 -0800

    .

---
+++ Shared/Commands/Root.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics.CodeAnalysis;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace TinyCA.Commands;
+
+[Command(Name = "tinica ", Description = "Tiny Certificate Authority")]
+[Subcommand(typeof(Init), typeof(Issue))]
+public class Root
+{
+    public const string Dir = ".tinyca";
+    public const string KeyFile = "key.pem";
+    public const string CertFile = "cert.pem";
+    public const string CertLabel = "CERTIFICATE";
+    public const string KeyLabel = "RSA PRIVATE KEY";
+
+    [SuppressMessage("Performance", "CA1822")]
+    public int OnExecute(CommandLineApplication app, IConsole console)
+    {
+        console.WriteLine("You must specify at a subcommand.");
+        app.ShowHelp();
+        return 1;
+    }
+
+}
